da51cdea4c314a0435aa77d6f92b1e809ae702d7
Marion3191 <fbsmarion@gmail.com>
2021-11-26 09:32:28 +0100

    envoi au local storage

--- P5-Dev-Web-Kanap-master/front/js/product.js
+++ P5-Dev-Web-Kanap-master/front/js/product.js
@@ -4,10 +4,12 @@
    displayArticle(article)
 })()
 
+//recuperation de L'ID
 function getArticleId() {
     return new URL(location.href).searchParams.get("id")
     }
 
+    //apell a l'API
 function getArticle(articleId) {
     return fetch(`http://localhost:3000/api/products/${articleId}`)
   
@@ -21,7 +23,7 @@ function getArticle(articleId) {
         alert(error)
     })
 }
-
+//Affichage d'un produit celon son ID
 function displayArticle(article) {
   document.querySelector("#title").innerHTML += `${article.name}`;
   document.querySelector("#description").innerHTML += `${article.description}`;
@@ -30,4 +32,44 @@ function displayArticle(article) {
   for(colors of article.colors){
       document.querySelector("#colors").innerHTML += `<option value="${colors}">${colors}</option>`;
   }
+
+//selection du btn ajouter l'article au panier
+const btnEnvoyer = document.querySelector("#addToCart");
+
+
+//ecouter le bouton et envoyer le panier
+btnEnvoyer.addEventListener("click", (Event)=>{
+    Event.preventDefault();
+
+//option de quantité et de couleur
+const couleurProduct = document.getElementById("colors").value;
+
+const quantiteProduct = quantity.value;
+
+// récuperation des choix client
+let optionsProduct = {
+    id_ProduitSelectionner: article._id,
+    couleur_Produit: couleurProduct,
+    quantite: quantiteProduct,
+}; 
+
+// variable "enregister dans la localstorage" (key , value)
+let stockage = JSON.parse(localStorage.getItem("kanape"));
+console.log(stockage);
+
+if(stockage){    
+    stockage.push(optionsProduct);
+    localStorage.setItem("kanape", JSON.stringify(stockage));    
+    console.log("if");
 }
+
+else{
+  stockage = [];
+  stockage.push(optionsProduct);
+  localStorage.setItem("kanape", JSON.stringify(stockage));  
+  console.log("else");
+}
+});
+}
+
+console.log(localStorage);
